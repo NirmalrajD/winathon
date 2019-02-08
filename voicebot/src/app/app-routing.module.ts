3025c3f72218103e70a713196a0451621a1ed749
NirmalRajD <thenirmalraj@gmail.com>
2019-02-08 12:13:58 +0530

    Angular Maatirel based Ui

--- voicebot/src/app/app-routing.module.ts
+++ voicebot/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminpanelComponent } from './adminpanel/adminpanel.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Admin', component: AdminpanelComponent },
+{ path: '',
+    redirectTo: '/Admin',
+    pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- voicebot/src/app/app.module.ts
+++ voicebot/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdminpanelComponent } from './adminpanel/adminpanel.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatPaginatorModule, MatTableModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminpanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+BrowserAnimationsModule, MatPaginatorModule, MatTableModule,
+MatButtonModule, MatCheckboxModule, MatTabsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
